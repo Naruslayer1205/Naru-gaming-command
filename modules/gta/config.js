1b18e11bc3cbaefcbd3981d59a19fe44b972dcd1
Naruslayer1205 <152826532+Naruslayer1205@users.noreply.github.com>
2026-09-08 13:37:02 +0200

    Update config.js

--- modules/gta/config.js
+++ modules/gta/config.js
@@ -1,20 +1,32 @@
 const path = require('path');
 
 module.exports = {
-  channelId: '1546773502336442420',
 
-  checkInterval: 15 * 60 * 1000,
+  channelId:
+    '1546773502336442420',
 
-  color: 0xF2A900,
+  checkInterval:
+    15 * 60 * 1000,
+
+  color:
+    0xF2A900,
 
   newswireUrl:
+    'https://www.rockstargames.com/newswire',
+
+  frenchNewswireUrl:
     'https://www.rockstargames.com/fr/newswire',
 
-  stateFile: path.join(
-    __dirname,
-    '..',
-    '..',
-    'data',
-    'gta-update-state.json'
-  )
+  maxCandidates:
+    25,
+
+  stateFile:
+    path.join(
+      __dirname,
+      '..',
+      '..',
+      'data',
+      'gta-update-state.json'
+    )
+
 };
